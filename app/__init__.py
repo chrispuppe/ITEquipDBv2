50164be8ee95b5d362ad0a35edab04bd5b21ca37
Chris Puppe <chrispuppe@gmail.com>
2021-11-03 14:03:50 -0700

    Style updates and code clean up

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from app import views, models
+
 # Create an Instance of Flask
 app = Flask(__name__)
 # Include config from config.py
@@ -10,3 +10,5 @@ app.secret_key = 's4asdgfkjagh2345nnlqnexiIS9732KksdnsdklkLKJjlksdfJLDF02418'
 # Create an instance of SQLAclhemy
 db = SQLAlchemy(app)
 Bootstrap(app)
+
+from app import views, models
