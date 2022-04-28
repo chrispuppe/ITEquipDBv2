c2d7237f7b162e9d98d8ea4b9125f6037139dd81
Chris Puppe <chrispuppe@gmail.com>
2022-04-28 14:48:27 -0700

    PDF Report open in browser

--- app/views.py
+++ app/views.py
@@ -359,12 +359,11 @@ def employee_report_pdf(id):
                                             assigned_printers=assigned_printers)
 
     css = 'app/static/css/bootstrap.css'
-    # pdfkit_config = pdfkit.configuration(wkhtmltopdf='/venv/lib/python3.6/site-packages/wkhtmltopdf/')
-    pdf_report = pdfkit.from_string(rendered_report, False, css=css) #, verbose=True, configuration=pdfkit_config)
 
+    pdf_report = pdfkit.from_string(rendered_report, False, css=css)
     pdf_response = flask.make_response(pdf_report)
     pdf_response.headers['Content-type'] = 'application/pdf'
-    pdf_response.headers['Content-Disposition'] = f'inline; {employee.name}.pdf'
+    pdf_response.headers['Content-Disposition'] = f'inline; filename={employee.name}.pdf'
 
     return pdf_response
 
